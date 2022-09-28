0ad915f89542f65d10cbed8a1a5f3c06d91a9e8b
juan9900 <juanluislauretta@gmail.com>
2022-09-28 15:27:17 -0400

    changed navbar and modal
    Added responsive navbar with functional hamburger button to toggle the navbar.
    
    Adjusted modal height in small devices (<500px width)

--- scripts.js
+++ scripts.js
@@ -1,19 +1,23 @@
 const links = {
     HypedStreet: {
         github: "https://github.com/juan9900/PaginaModa",
-        website: 'https://juan9900.github.io/PaginaModa/'
+        website: 'https://juan9900.github.io/PaginaModa/',
+        description: 'Static website about trending clothes made with HTML + CSS + JS with the fullpage.js library.'
     },
     F1: {
         github: "https://github.com/juan9900/f1",
-        website: 'https://juan9900.github.io/f1/'
+        website: 'https://juan9900.github.io/f1/',
+        description: 'Static website about F1 data like, last results, next race, current positions and some historic data made with HTML + CSS '
     },
     Bienes: {
         github: "https://github.com/juan9900/BienesRaices",
-        website: 'https://juan9900.github.io/BienesRaices/'
+        website: 'https://juan9900.github.io/BienesRaices/',
+        description: 'Static website for finding/buying a house and read some blog articles made with HTML + CSS'
     },
     QuizUp: {
         github: 'https://github.com/juan9900/QuizUp',
-        website: 'https://juan9900.github.io/QuizUp/'
+        website: 'https://juan9900.github.io/QuizUp/',
+        description: 'Small game inspired in Trivia Crack made with HTML + CSS + JS with api calls and AJAX'
     }
 }
 
@@ -26,7 +30,7 @@ const contactForm = document.getElementById('contact-form');
 window.onscroll = function () { myFunction() };
 
 // Get the navbar
-var navbar = document.getElementById("navbar");
+var navbar = document.querySelector("header");
 
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;
@@ -49,9 +53,9 @@ const linksContainers = [
     document.querySelector('#btn-contact'),
 ];
 
+//Scroll to section when click in navbar link
 linksContainers.forEach(container => {
     container.addEventListener('click', function (e) {
-        console.log('boton');
         const link = e.target.closest('.nav-link');
         if (!link) return;
         e.preventDefault();
@@ -71,53 +75,29 @@ linksContainers.forEach(container => {
     });
 });
 
-/*
 
-$('contact-form').addEventListener('submit', (e) => {
-    // e.preventDefault();
-    // let xhr = new XMLHttpRequest();
-    // xhr.open("POST", "https://formsubmit.co/829b4e1afbc9484f01f5d68ec30f3c57");
-
-    // xhr.setRequestHeader("Accept", "application/json");
-    // xhr.setRequestHeader("Content-Type", "application/json");
-
-    // const name = contactForm.elements.formName.value;
-    // const email = contactForm.elements.formEmail.value
-    // const message = contactForm.elements.formMessage.value
-    // xhr.onload = () => console.log();
-
-    // let data = `{
-    // "Name": ${name},
-    // "Email": ${email},
-    // "Message": ${message};
-    // }`;
-
-    // xhr.send(data)
-    console.log('enviado')
-    e.preventDefault();
-    // fetch("https://formsubmit.co/ajax/829b4e1afbc9484f01f5d68ec30f3c57", {
-    //     method: "POST",
-    //     headers: {
-    //         'Content-Type': 'application/json',
-    //         'Accept': 'application/json'
-    //     },
-    //     body: JSON.stringify({
-    //         name: 'nombre'
-    //         // $('#contact-form').elements.formName.value
-    //         ,
-    //         email: 'email'
-    //         // $('#contact-form').elements.formEmail.value
-    //         ,
-    //         message: 'mensaje'
-    //         // $('#contact-form').elements.formMessage.value
-    //     })
-    // })
-    //     .then(response => response.json())
-    //     .then(data => console.log(data))
-    //     .catch(error => console.log(error));
-
+//Toggle mobile navbar
+$('#button-open-navbar').click(function () {
+    toggleNavbar();
 })
-*/
+
+$('#button-close-navbar').click(function () {
+
+    toggleNavbar();
+})
+
+
+function toggleNavbar() {
+    $('#navbar').toggleClass('mobile-active');
+    $('#button-open-navbar').toggleClass('invisible');
+    $('#button-close-navbar').toggleClass('invisible');
+}
+
+
+//Close mobile navbar when touching a link
+$('.nav-link').click(function (e) {
+    toggleNavbar();
+})
 
 $('#contact-form').submit(function (e) {
     e.preventDefault();
@@ -175,15 +155,16 @@ $(document).ready(function () {
 
 $('.button-know-more').click(function (e) {
 
-    const websiteDesc = $(this).parent().siblings('h4').html();
+    const websiteDesc = $(this).parent().siblings('h4').text();
     const websiteName = websiteDesc.split(" ")[0].replace(/\s/g, '');
     console.log(websiteName);
-    $('#modal-work-title').html(websiteDesc)
+    $('#modal-work-title').text(websiteDesc)
 
 
 
     for (var link in links) {
         if (websiteName === link) {
+            $('#modal-web-description').text(links[link].description);
             $('#modal-github-link').attr('href', links[link].github);
             $('#modal-website-link').attr('href', links[link].website);
         }
